c0de8e013c8a915b3860808e356c4f09a943045a
amkrajewski <ak@psu.edu>
2023-09-07 20:39:47 -0400

    - added presence matrix generation from text file with lists of elements (one alloy per row)

--- nimcso.nim
+++ nimcso.nim
@@ -1,20 +1,36 @@
 # Copyrigth (C) 2023 Adam M. Krajewski
 
-import math
 import std/sugar
 import arraymancer
 import strutils
 import times, os
 
-proc preventedData(elList: Tensor[uint8]): int =
-    let a = [[1,0,0,0,1], [1,0,0,1,0], [1,1,1,1,1], [0,0,0,0,0],
-        [1,0,0,0,1], [1,0,0,1,0], [1,1,1,1,1], [0,0,0,0,0],
-        [1,0,0,0,1], [1,0,0,1,0], [1,1,1,1,1], [0,0,0,0,0],
-        [1,0,0,0,1], [1,0,0,1,0], [1,1,1,1,1], [0,0,0,0,0],
-        [1,0,0,0,1], [1,0,0,1,0], [1,1,1,1,1], [0,0,0,0,0] ].toTensor().asType(uint8)
-    var c = a *. elList
+proc preventedData(elList: Tensor[uint8], presenceMatrix: Tensor[uint8]): int =
+    var c = presenceMatrix *. elList
     c = c.max(axis=1)
     result = c.asType(int).sum()
+
+proc getPresenceMatrix(): Tensor[uint8] =
+    let elementOrder = ["Fe", "Cr","Cr","Ni","Co","Al","Ti","Nb","Cu","Mo","Ta","Zr",
+                        "V'","Hf","W'","Mn","Si","Re","B'","Ru","C'","Sn","Mg","Zn",
+                        "Li","O'", "Y'", "Pd","N'", "Ca","Ir","Sc","Ge","Be","Ag",
+                        "Nd","S'", "Ga"]
+
+    let elementsPresentList = readFile("elementList.txt").splitLines()
+    var
+        presence = newTensor[uint8]([elementsPresentList.len, elementOrder.len])
+        lineN: int = 0
+        elN: int = 0
+
+    for line in elementsPresentList:
+        let elements = line.split(",")
+        elN = 0
+        for el in elementOrder:
+            if elements.contains(el):
+                presence[lineN, elN] = 1
+            elN += 1
+        lineN += 1
+    result = presence
 
 template benchmark(benchmarkName: string, code: untyped) =
   block:
@@ -25,10 +41,15 @@ template benchmark(benchmarkName: string, code: untyped) =
     echo "CPU Time [", benchmarkName, "] ", elapsedStr, "s"
 
 when isMainModule:
-    let b = [[0, 1, 1, 1, 0]].toTensor().astype(uint8)
+    
+    let presenceMatrix = getPresenceMatrix()
+
+    let b = [[0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0]].toTensor().astype(uint8)
+
     benchmark "my benchmark":
-        for i in 0..1000000:
-            discard preventedData(b)
+        for i in 0..1000:
+            discard preventedData(b, presenceMatrix)
+    #echo preventedData(b, presenceMatrix)
 
 
 
