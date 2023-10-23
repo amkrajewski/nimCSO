edbc060666d7cc6c9cc2c469b63d59d8f42bf805
amkrajewski <ak@psu.edu>
2023-10-23 13:56:50 -0400

    - code style improvements for bitArray submodule

--- nimcso/bitArrayAutoconfigured.nim
+++ nimcso/bitArrayAutoconfigured.nim
@@ -14,146 +14,146 @@ type Config = object
     elementOrder: seq[string]
     datasetPath: string
 
-const 
-  configPath {.strdefine.}: string = "config.yaml"
-  config = static:
-    var config: Config
-    let s = readFile(configPath)
-    load(s, config)
-    config
+const
+    configPath {.strdefine.}: string = "config.yaml"
+    config = static:
+        var config: Config
+        let s = readFile(configPath)
+        load(s, config)
+        config
 
-  elementOrder = config.elementOrder
-  elementN = elementOrder.len
+    elementOrder = config.elementOrder
+    elementN = elementOrder.len
 
 func divUp(a, b: int): int =
-  ## Like div, but rounds up instead of down.
-  let extra = if a mod b > 0: 1 else: 0
+    ## Like div, but rounds up instead of down.
+    let extra = if a mod b > 0: 1 else: 0
   return a div b + extra
 
 const lenInt64 = elementN.divUp(64)
 echo "Using ", lenInt64, " uint64s to store ", elementN, " elements."
 
 type BitArray* = object
-  ## Creates an array of bits all packed in together.
-  bits*: array[lenInt64, uint64]
+    ## Creates an array of bits all packed in together.
+    bits*: array[lenInt64, uint64]
 
 when defined(release):
-  {.push checks: off.}
+    {.push checks: off.}
 
 func firstFalse*(b: BitArray): (bool, int) =
-  for i in 0 ..< lenInt64:
-    if b.bits[i] == 0:
-      return (true, i * 64)
-    if b.bits[i] != uint64.high:
-      let matchingBits = firstSetBit(not b.bits[i])
-      return (true, i * 64 + matchingBits - 1)
-  (false, 0)
+    for i in 0 ..< lenInt64:
+        if b.bits[i] == 0:
+            return (true, i * 64)
+        if b.bits[i] != uint64.high:
+            let matchingBits = firstSetBit(not b.bits[i])
+            return (true, i * 64 + matchingBits - 1)
+    (false, 0)
 
 func unsafeGet*(b: BitArray, i: int): bool =
-  ## Access a single bit (unchecked).
-  let
-    bigAt = i div 64
-    littleAt = i mod 64
-    mask = 1.uint64 shl littleAt
-  return (b.bits[bigAt] and mask) != 0
+    ## Access a single bit (unchecked).
+    let
+        bigAt = i div 64
+        littleAt = i mod 64
+        mask = 1.uint64 shl littleAt
+    return (b.bits[bigAt] and mask) != 0
 
 func unsafeSetFalse*(b: var BitArray, i: int) =
-  ## Set a single bit to false (unchecked).
-  let
-    bigAt = i div 64
-    littleAt = i mod 64
-    mask = 1.uint64 shl littleAt
-  b.bits[bigAt] = b.bits[bigAt] and (not mask)
+    ## Set a single bit to false (unchecked).
+    let
+        bigAt = i div 64
+        littleAt = i mod 64
+        mask = 1.uint64 shl littleAt
+    b.bits[bigAt] = b.bits[bigAt] and (not mask)
 
 func unsafeSetTrue*(b: var BitArray, i: int) =
-  ## Set a single bit to true (unchecked).
-  let
-    bigAt = i div 64
-    littleAt = i mod 64
-    mask = 1.uint64 shl littleAt
-  b.bits[bigAt] = b.bits[bigAt] or mask
+    ## Set a single bit to true (unchecked).
+    let
+        bigAt = i div 64
+        littleAt = i mod 64
+        mask = 1.uint64 shl littleAt
+    b.bits[bigAt] = b.bits[bigAt] or mask
 
 when defined(release):
-  {.pop.}
+    {.pop.}
 
 func `[]`*(b: BitArray, i: int): bool =
-  ## Access a single bit.
-  if i < 0 or i >= elementN:
-    raise newException(IndexDefect, "Index out of bounds")
-  b.unsafeGet(i)
+    ## Access a single bit.
+    if i < 0 or i >= elementN:
+        raise newException(IndexDefect, "Index out of bounds")
+    b.unsafeGet(i)
 
 func `[]=`*(b: var BitArray, i: int, v: bool) =
-  # Set a single bit.
-  if i < 0 or i >= elementN:
-    raise newException(IndexDefect, "Index out of bounds")
-  if v:
-    b.unsafeSetTrue(i)
-  else:
-    b.unsafeSetFalse(i)
+    # Set a single bit.
+    if i < 0 or i >= elementN:
+        raise newException(IndexDefect, "Index out of bounds")
+    if v:
+        b.unsafeSetTrue(i)
+    else:
+        b.unsafeSetFalse(i)
 
 func `==`*(a, b: var BitArray): bool =
-  ## Are two bit arrays the same.
-  for i in 0 ..< lenInt64:
-    if a.bits[i] != b.bits[i]:
-      return false
-  return true
+    ## Are two bit arrays the same.
+    for i in 0 ..< lenInt64:
+        if a.bits[i] != b.bits[i]:
+            return false
+    return true
 
 func `and`*(a, b: BitArray): BitArray =
-  ## And(s) two bit arrays returning a new bit array.
-  result = BitArray()
-  for i in 0 ..< lenInt64:
-    result.bits[i] = a.bits[i] and b.bits[i]
+    ## And(s) two bit arrays returning a new bit array.
+    result = BitArray()
+    for i in 0 ..< lenInt64:
+        result.bits[i] = a.bits[i] and b.bits[i]
 
 func `or`*(a, b: BitArray): BitArray =
-  ## Or(s) two bit arrays returning a new bit array.
-  result = BitArray()
-  for i in 0 ..< lenInt64:
-    result.bits[i] = a.bits[i] or b.bits[i]
+    ## Or(s) two bit arrays returning a new bit array.
+    result = BitArray()
+    for i in 0 ..< lenInt64:
+        result.bits[i] = a.bits[i] or b.bits[i]
 
 func `not`*(a: BitArray): BitArray =
-  ## Not(s) or inverts a and returns a new bit array.
-  result = BitArray()
-  for i in 0 ..< lenInt64:
-    result.bits[i] = not a.bits[i]
+    ## Not(s) or inverts a and returns a new bit array.
+    result = BitArray()
+    for i in 0 ..< lenInt64:
+        result.bits[i] = not a.bits[i]
 
 func `$`*(b: BitArray): string =
-  ## Turns the bit array into a string.
-  result = newStringOfCap(elementN)
-  for i in 0 ..< elementN:
-    if b[i]:
-      result.add "1"
-    else:
-      result.add "0"
+    ## Turns the bit array into a string.
+    result = newStringOfCap(elementN)
+    for i in 0 ..< elementN:
+        if b[i]:
+            result.add "1"
+        else:
+            result.add "0"
 
 func count*(b: BitArray): int =
-  ## Returns the number of bits set.
-  for i in 0 ..< lenInt64:
-    result += countSetBits(b.bits[i])
+    ## Returns the number of bits set.
+    for i in 0 ..< lenInt64:
+        result += countSetBits(b.bits[i])
 
 func clear*(b: var BitArray) =
-  ## Unsets all of the bits.
-  for i in 0 ..< lenInt64:
-    b.bits[i] = 0
+    ## Unsets all of the bits.
+    for i in 0 ..< lenInt64:
+        b.bits[i] = 0
 
 func hash*(b: BitArray): Hash =
-  ## Computes a Hash for the bit array.
-  hash(b.bits)
+    ## Computes a Hash for the bit array.
+    hash(b.bits)
 
 iterator items*(b: BitArray): bool =
-  for i in 0 ..< elementN:
-    yield b[i]
+    for i in 0 ..< elementN:
+        yield b[i]
 
 iterator pairs*(b: BitArray): (int, bool) =
-  for i in 0 ..< elementN:
-    yield (i, b[i])
+    for i in 0 ..< elementN:
+        yield (i, b[i])
 
 func toBoolArray*(b: BitArray): array[elementN, bool] =
-  ## Converts the bit array to a sequence of bools.
-  for i in 0 ..< elementN:
-    result[i] = b.unsafeGet(i)
+    ## Converts the bit array to a sequence of bools.
+    for i in 0 ..< elementN:
+        result[i] = b.unsafeGet(i)
 
 func toSetPositions*(b: BitArray): seq[int] =
-  ## Converts the bit array to a set of positions.
-  for i in 0 ..< elementN:
-    if b.unsafeGet(i):
-      result.add i
+    ## Converts the bit array to a set of positions.
+    for i in 0 ..< elementN:
+        if b.unsafeGet(i):
+            result.add i
