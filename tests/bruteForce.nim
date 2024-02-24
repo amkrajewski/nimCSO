3eae797198270c3698166334275d73e683dbfc49
Adam M. Krajewski <54290107+amkrajewski@users.noreply.github.com>
2024-02-24 15:43:38 -0500

    (tests) added bit/bool array consistency check within the bruteFoce test

--- tests/bruteForce.nim
+++ tests/bruteForce.nim
@@ -6,7 +6,6 @@ import ../src/nimcso
 suite "Corectness Tests":
     setup:
         let pBitA = getPresenceBitArrays()
-        let pBoolA = getPresenceBoolArrays() # 
 
         var referenceResult: seq[ElSolution]
 
@@ -18,11 +17,25 @@ suite "Corectness Tests":
             var elSol = newElSolution(elList, pBitA)
             elSol.prevented = prevented
             referenceResult.add(elSol)
+    
+    test "Verify Bit/Bool Array Backend Consistency":
+        let pBoolA = getPresenceBoolArrays()
+        var referenceResult_BoolA: seq[ElSolution]
 
+        for line in readFile("tests/bruteForce_ref.csv").splitLines():
+            let 
+                parts = line.split(",")
+                elList = parts[0].strip().split('-')
+                prevented = parts[2].strip().parseInt()
+            var elSol = newElSolution(elList, pBoolA)
+            elSol.prevented = prevented
+            referenceResult_BoolA.add(elSol)
+        
+        for i in 0 ..< referenceResult.len:
+            check referenceResult[i] == referenceResult_BoolA[i]
 
     test "Brute Force Suite Complete":
-        let result1 = leastPreventing()
-        let result = bruteForce()
+        let result = bruteForce(verbose=false)
 
         for i in 1 ..< result.len:
             test $result[i]:
@@ -30,4 +43,4 @@ suite "Corectness Tests":
             test "^ Correctly prevents " & $referenceResult[i-1].prevented:
                 check result[i].prevented == referenceResult[i-1].prevented
 
-        saveResults(result, "bruteForce.csv", "-")
+        saveResults(result, "tests/bruteForce_testResult.csv", "-")
