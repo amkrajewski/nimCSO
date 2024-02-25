0ce4a55762c6d35ae61f7dc64e52dbc3be08e2f7
Adam M. Krajewski <54290107+amkrajewski@users.noreply.github.com>
2024-02-25 11:20:36 -0500

    (bitarray) a lot of docstring updates

--- src/nimcso/bitArrayAutoconfigured.nim
+++ src/nimcso/bitArrayAutoconfigured.nim
@@ -31,7 +31,7 @@ func divUp(a, b: int): int =
     let extra = if a mod b > 0: 1 else: 0
     return a div b + extra
 
-const lenInt64 = elementN.divUp(64)
+const lenInt64* = elementN.divUp(64) ## **Compile-time constant** of the number of ``uint64`` in the array used to store elements in the ``BitArray``.
 styledEcho "Using ", styleBright, fgMagenta, $lenInt64, " uint64s", resetStyle, " to store ", styleBright, fgMagenta, $elementN, resetStyle, " elements."
 # Side Note: There is a good reason to use uint64 even if the problem is smaller. The reason is performance. A 64-bit CPU is much better at handling 64-bit numbers; thus even if you hard-code the type 
 # of the bit array values to be uint8 or uint16, the compiler will likely align them to fill 64-bit boundaries anyways, so there won't be any memory savings, while it will be slightly slower. E.g.,
@@ -46,6 +46,7 @@ when defined(release):
     {.push checks: off.}
 
 func firstFalse*(b: BitArray): (bool, int) =
+    ## Returns the value and position of the first false bit in [BitArray].
     for i in 0 ..< lenInt64:
         if b.bits[i] == 0:
             return (true, i * 64)
@@ -55,7 +56,7 @@ func firstFalse*(b: BitArray): (bool, int) =
     (false, 0)
 
 func unsafeGet*(b: BitArray, i: int): bool =
-    ## Access a single bit (unchecked).
+    ## Access a single bit (unchecked) of the [BitArray] at position ``i``.
     let
         bigAt = i div 64
         littleAt = i mod 64
@@ -63,7 +64,7 @@ func unsafeGet*(b: BitArray, i: int): bool =
     return (b.bits[bigAt] and mask) != 0
 
 func unsafeSetFalse*(b: var BitArray, i: int) =
-    ## Set a single bit to false (unchecked).
+    ## Set a single bit to ``false`` (unchecked) of the [BitArray] at position ``i``.
     let
         bigAt = i div 64
         littleAt = i mod 64
@@ -71,7 +72,7 @@ func unsafeSetFalse*(b: var BitArray, i: int) =
     b.bits[bigAt] = b.bits[bigAt] and (not mask)
 
 func unsafeSetTrue*(b: var BitArray, i: int) =
-    ## Set a single bit to true (unchecked).
+    ## Set a single bit to ``true`` (unchecked) of the [BitArray] at position ``i``.
     let
         bigAt = i div 64
         littleAt = i mod 64
@@ -82,13 +83,13 @@ when defined(release):
     {.pop.}
 
 func `[]`*(b: BitArray, i: int): bool =
-    ## Access a single bit.
+    ## Access a single bit of the [BitArray] at position ``i``, with bounds checking.
     if i < 0 or i >= elementN:
         raise newException(IndexDefect, "Index out of bounds")
     b.unsafeGet(i)
 
 func `[]=`*(b: var BitArray, i: int, v: bool) =
-    # Set a single bit.
+    # Set a single bit of the [BitArray] at position ``i``, with bounds checking.
     if i < 0 or i >= elementN:
         raise newException(IndexDefect, "Index out of bounds")
     if v:
@@ -97,40 +98,40 @@ func `[]=`*(b: var BitArray, i: int, v: bool) =
         b.unsafeSetFalse(i)
 
 func `==`*(a, b: var BitArray): bool =
-    ## Are two bit arrays the same.
-    for i in 0 ..< lenInt64:
+    ## Checks equality of two [BitArray]s by comparing ``unit64`` values in their ``bits`` fields.
+    for i in 0..<lenInt64:
         if a.bits[i] != b.bits[i]:
             return false
     return true
 
 func `and`*(a, b: BitArray): BitArray =
-    ## And(s) two bit arrays returning a new bit array.
+    ## Applies ``AND`` over all positions of two [BitArray]s returning a new [BitArray].
     result = BitArray()
-    for i in 0 ..< lenInt64:
+    for i in 0..<lenInt64:
         result.bits[i] = a.bits[i] and b.bits[i]
 
 func `or`*(a, b: BitArray): BitArray =
-    ## Or(s) two bit arrays returning a new bit array.
+    ## Applies ``OR`` over all positions of two [BitArray]s returning a new [BitArray].
     result = BitArray()
-    for i in 0 ..< lenInt64:
+    for i in 0..<lenInt64:
         result.bits[i] = a.bits[i] or b.bits[i]
 
 func `not`*(a: BitArray): BitArray =
-    ## Not(s) or inverts a and returns a new bit array.
+    ## Applies ``NOT`` over all positions of a [BitArray] returning a new [BitArray].
     result = BitArray()
-    for i in 0 ..< lenInt64:
+    for i in 0..<lenInt64:
         result.bits[i] = not a.bits[i]
 
 func `$`*(b: BitArray): string =
-    ## Turns the bit array into a string.
+    ## Returns a string representation of the [BitArray] as a string of ``0``s and ``1``s.
     result = newStringOfCap(elementN)
-    for i in 0 ..< elementN:
+    for i in 0..<elementN:
         if b[i]:
             result.add "1"
         else:
             result.add "0"
 
-proc styledPrint*(b: BitArray) =
+proc styledEcho*(b: BitArray) =
     ## Prints the [BitArray] in a stylish way, with ``1``s in bright green and ``0``s in dim red.
     for i in 0..<elementN:
         if b[i]:
@@ -139,7 +140,7 @@ proc styledPrint*(b: BitArray) =
             stdout.styledWrite styleDim, fgRed, "0"
     styledEcho resetStyle
 
-proc styledPrintAnnotated*(b: BitArray) =
+proc styledEchoAnnotated*(b: BitArray) =
     ## Prints the [BitArray] in a stylish way, with (1) top annotation of the index like ``| 0| 1| 2| ... |10|11| ...``, (2) side annotation of the lenght of the array, 
     ## and (3) ``1``s in bright green and ``0``s in dim red.
     # Top annotation
