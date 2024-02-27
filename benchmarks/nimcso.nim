7508acb3ba20ca4b2f9c61773219ce6e808767f7
Adam M. Krajewski <54290107+amkrajewski@users.noreply.github.com>
2024-02-27 12:14:12 -0500

    (bench) added `bruteForceInt` to benchmarking suite

--- benchmarks/nimcso.nim
+++ benchmarks/nimcso.nim
@@ -31,3 +31,24 @@ block:
     let elapsedStrPC = elapsedPC.formatFloat(format = ffDecimal, precision = 1)
     styledEcho "CPU Time [per comparison] ", styleBright, fgGreen, elapsedStrPC, "ns\n", resetStyle
 
+block:
+    styledEcho fgBlue, "\nCount Data Points Prevented by Removal of Cr-Fe-Ni-Co-Al (nimCSO Int Representation)\n", resetStyle
+
+    let presenceIntArray = getPresenceIntArray()
+    let a = 0b00000000000000000000000000000000_00000000000000000000000000111111'u64
+    assert preventedData(a, presenceIntArray) == 1955
+
+    let memoryUsed = (presenceIntArray.sizeof.float / 1024).formatFloat(format = ffDecimal, precision = 1)
+    echo "Memory to Store Dataset:", memoryUsed, "kB"
+
+    let t0 = epochTime()
+    for i in 1..100000:
+        block:
+            let b = 0b00000000000000000000000000000000_00000000000000000000000000111111'u64
+            discard preventedData(b, presenceIntArray)
+    let elapsed = (epochTime() - t0) * 10
+    let elapsedStr = elapsed.formatFloat(format = ffDecimal, precision = 3)
+    styledEcho "CPU Time [per dataset evaluation] ", styleBright, fgGreen, elapsedStr, "μs", resetStyle
+    let elapsedPC = elapsed / nimcso.alloyN * 1000
+    let elapsedStrPC = elapsedPC.formatFloat(format = ffDecimal, precision = 3)
+    styledEcho "CPU Time [per comparison] ", styleBright, fgGreen, elapsedStrPC, "ns", resetStyle
