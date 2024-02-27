071b6d031cca5a6994aed0ef8e991d0d3ac7c328
Adam M. Krajewski <54290107+amkrajewski@users.noreply.github.com>
2024-02-27 12:22:26 -0500

    (tests) updated the `runAll` convenience script

--- tests/runAll.nim
+++ tests/runAll.nim
@@ -1,3 +1,8 @@
+# nimCSO Library
 import ../src/nimcso
+# Benchmark scripts in the order they will be executed in
 import benchmarksRuntime
+import leastPreventing
 import bruteForce
+import bruteForceInt
+import algorithmSearch
